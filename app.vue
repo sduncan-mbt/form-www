c3e593eaa5d4d4e4b7123cef0467caae6f77be99
Stephen <stephen@manchesterbusiness.tech>
2022-10-12 04:17:11 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <form @click.prevent="formSubmitted=true">
+      <input type="text" />
+    </form>
+    <h1>{{formSubmitted}}</h1>
+  </div>
+</template>
+<script setup>
+import {useState} from "nuxt/app";
+
+const formSubmitted = useState('formSubmitted', () => false)
+</script>
